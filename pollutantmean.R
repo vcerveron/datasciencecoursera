8d884e8cb48c1202fef07fb43233650996617871
vcerveron <cerveron@uv.es>
2015-05-15 20:09:44 +0200

    limpieza

--- pollutantmean.R
+++
@@ -1,8 +0,0 @@
-pollutantmean <- function(directory, pollutant, id=1:332) {
-  data <- NULL
-  for(idn in id) {
-    datap <- read.csv(paste0(directory,"/",formatC(idn,width=3,flag="0"),".csv"))
-    data <- rbind(data, datap)
-  }
-  mean(data[,pollutant], na.rm=TRUE)
-}
